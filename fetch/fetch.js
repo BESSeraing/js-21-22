3f50c17ab27f7ad166786cb5a52d6ca939ce0c36
Jona <jona303@gmail.com>
2022-02-25 12:06:22 +0100

    add comments button

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,3 +1,8 @@
+let headers = new Headers();
+headers.append('Accept', 'application/json');
+
+var requestMeta = {headers: headers};
+
 document.getElementById('refreshButton').addEventListener('click', function() {
     fetchBlogPosts();
 });
@@ -7,11 +12,6 @@ function fetchBlogPosts() {
     let errorContainer = document.getElementById('errors');
     errorContainer.style.display = "none";
     document.getElementById('result').innerHTML = '';
-
-    let headers = new Headers();
-    headers.append('Accept', 'application/json');
-
-    var requestMeta = {headers: headers};
 
     fetch("https://prof2.bes-webdeveloper-seraing.be/blog/back/api/blog_posts?page=1", requestMeta)
         .then(function(httpResponse) {
@@ -31,7 +31,29 @@ function fetchBlogPosts() {
                 article.classList.add('list-group-item');
                 article.innerHTML = '<h2>' + blogPost.title + '</h2>';
                 article.innerHTML += '<p>' + blogPost.body + '</p>';
+                article.innerHTML += '<button class="btn btn-primary fetchComment" data-post-id="'+blogPost.id+'">Voir les commentaires</button>';
                 document.getElementById('result').appendChild(article);
+            }
+            let buttons = document.querySelectorAll('.fetchComment');
+            for (button of buttons) {
+                button.addEventListener('click', function(e){
+                    fetchPostComments(e.target.getAttribute('data-post-id'))
+                        .then(function(comments){
+                            if (comments) {
+                                let commentsHtml = document.createElement('ul');
+                                commentsHtml.classList.add('list-group');
+                                for(comment of comments) {
+                                    let li = document.createElement('li');
+                                    li.classList.add('list-group-item');
+                                    li.innerText = comment.user + ' - ' + comment.content;
+                                    commentsHtml.appendChild(li);
+                                }
+                                e.target.parentElement.append(commentsHtml);
+                                e.target.remove();
+                            }
+
+                        });
+                });
             }
         })
         .catch(function(err){
@@ -46,3 +68,14 @@ function fetchBlogPosts() {
         document.getElementById('result').appendChild(beforeResult);
 }
 
+
+function fetchPostComments(postId) {
+    return fetch("https://prof2.bes-webdeveloper-seraing.be/blog/back/api/comments?post.id="+postId+"&page=1", requestMeta)
+        .then(function(httpResponse) {
+            if(httpResponse.status !== 200) {
+                throw new Error("request failed");
+            } else {
+                return httpResponse.json();
+            }
+        });
+}
